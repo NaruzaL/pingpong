4f1002346e6872a1393e3a06002e262ce9ae53cd
PETE LAZURAN <pdlazuran@gmail.com>
2017-05-05 13:49:44 -0700

    populate li list on HTML. Filter functions not working.

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,36 @@
 //backend logic------------------------------------------
 
-//pingpong will take the users number and create an array that starts at 1 and fill in up to the users number. Then it sends the array through the filter function to replace 3's, 5's and 15's with the corrisponding words and finally returns them in another empty array.
+//pingpong will take the users number and create an array that starts at 1 and fill in up to the users number. Then it sends the created array through the filter functions to replace 3's, 5's and 15's with the corrisponding words and finally returns them in another empty array.
 debugger;
-var displayArray = [];
-var numberArray = [];
+var filteredArray = [];
+
 var pingpong = function(numbers, i){
+  var numberArray = [];
   for (var i = 1; i <= numbers; i++){
   numberArray.push(i);
+  }
   console.log(numberArray);
-  filter(numberArray[i]);
-  displayArray.push(result);
+  for (var i = 0; i < numberArray.length; i++){
+  if (filter3(numberArray, i) === 0 && filter5(numberArray, i) === 0){
+    filteredArray.push("ping-pong");
   }
-  return displayArray;
-}
-
-var filter = function(numberArray){
-  var result = []
-  for (i = 1; i < numberArray.length; i++){
-  if (filter3(numberArray) === 0 && filter5 === 0){
-    numberArray.slice(numberArray[i]);
-    numberArray.push("ping-pong");
-  }
-    else if (filter3(numberArray) === 0){
-      numberArray.slice(numberArray[i]);
-      numberArray.push("ping");
+    else if (filter3(numberArray, i) === 0){
+      filteredArray.push("ping");
     }
-    else if (filter5(numberArray) === 0){
-      numberArray.slice(numberArray[i]);
+    else if (filter5(numberArray, i) === 0){
       filteredArray.push("pong");
     }
+    else {
+    filteredArray.push(i);
+    }
   }
-  return result;
+  return numberArray;
 }
 
-var filter3 = function (numberArray, i){
+function filter3 (numberArray, i){
   var check = (numberArray[i] % 3);
 }
-var filter5 = function (numberArray, i){
+function filter5 (numberArray, i){
   var check = (numberArray[i] % 5);
 }
 //UI logic--------------------------------------------------
@@ -45,8 +39,8 @@ $(document).ready(function(){
   $("#mainform").submit(function(event){
   var numbers = parseInt($("input#number").val());
   var result = pingpong(numbers);
-  for (i = 0; i <= displayArray.length; i++){
-  $("#output").append("<li>" + displayArray[i] + " </li>");
+  for (i = 0; i <= filteredArray.length; i++){
+  $("#output").append("<li>" + filteredArray[i] + " </li>");
   }
   event.preventDefault();
   });
