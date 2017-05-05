4a42136d0806e8458165c97f6e6e8caa6a5b23b5
PETE LAZURAN <pdlazuran@gmail.com>
2017-05-05 08:46:34 -0700

    completes basic HTML setup with form and area for output to display. JS front end logic parseInt user input for backend use.

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+//backend logic
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//UI logic
+
+$(document).ready(function(){
+  $("#mainform").submit(function(event){
+  var number = parseInt($("input#number").val());
+event.preventDefault();
+  });
+});
