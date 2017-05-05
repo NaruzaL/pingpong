bfb7bb6b9f137f2a238240ba96f200c6adc703bf
NaruzaL <pdlazuran@gmail.com>
2017-05-05 15:21:14 -0700

    replaces % functions with inline code

--- js/scripts.js
+++ js/scripts.js
@@ -1,47 +1,40 @@
 //backend logic------------------------------------------
 
 //pingpong will take the users number and create an array that starts at 1 and fill in up to the users number. Then it sends the created array through the filter functions to replace 3's, 5's and 15's with the corrisponding words and finally returns them in another empty array.
-debugger;
 var filteredArray = [];
 
-var pingpong = function(numbers, i){
+var pingpong = function(numbers){
   var numberArray = [];
   for (var i = 1; i <= numbers; i++){
   numberArray.push(i);
   }
-  console.log(numberArray);
-  for (var i = 0; i < numberArray.length; i++){
-  if (filter3(numberArray, i) === 0 && filter5(numberArray, i) === 0){
+  for (var i = 1; i <= numberArray.length; i++){
+  if ((i % 3 === 0) && (i % 5 === 0)){
     filteredArray.push("ping-pong");
   }
-    else if (filter3(numberArray, i) === 0){
+    else if (i % 3 === 0){
       filteredArray.push("ping");
-    }
-    else if (filter5(numberArray, i) === 0){
+    } 
+    else if (i % 5 === 0){
       filteredArray.push("pong");
     }
       else {
-      filteredArray.push(numberArray[i]);
+      filteredArray.push(i);
       }
   }
   return filteredArray;
 }
 
-function filter3 (numberArray, i){
-  var check = (numberArray[i] % 3);
-}
-function filter5 (numberArray, i){
-  var check = (numberArray[i] % 5);
-}
 //UI logic--------------------------------------------------
 
 $(document).ready(function(){
   $("#mainform").submit(function(event){
-  var numbers = parseInt($("input#number").val());
-  var result = pingpong(numbers);
-  for (i = 0; i <= filteredArray.length; i++){
-  $("#output").append("<li>" + filteredArray[i] + " </li>");
-  }
   event.preventDefault();
+  var numbers = parseInt($("input#number").val());
+  pingpong(numbers);
+  $("#output").empty();
+  filteredArray.forEach(function(i){
+  $("#output").append("<li>" + i + " </li>");
+  });
   });
 });
