108127ebcf43bbdea6209433c2c827d261f01fd5
PETE LAZURAN <pdlazuran@gmail.com>
2017-05-05 09:05:22 -0700

    create an array of numbers between 1 and the users number. Add functions to catch 3,5,15 in the array.

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,24 @@
 //backend logic
 
+//countingArray will count from 1 to the number selected by the user and fill the array with those numbers.
+var countingArray =
+  for (var i = 1; i < number; i++){
+    countingArray.push(i);
+  }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// catch3 will be used to scan each i in the countingArray and see if it's exactly divisable by three. catch5 will do the same for 5's, catch15 for 15's.
+function catch3(countingArray, i){
+  var temp = (array[i] % 3)
+  return temp;
+}
+function catch5(countingArray, i){
+  var temp = (array[i] % 5)
+  return temp;
+}
+function catch15(countingArray, i){
+  var temp = (array[i] % 15)
+  return temp;
+}
 
 
 
